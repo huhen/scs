cde9fedca6cfb96dc0eef5fdc8963b986aa7f980
Ivan <kinetic@bk.ru>
2016-04-14 10:40:30 +0300

    Remove Unused Librarys

--- src/Scs/Properties/AssemblyInfo.cs
+++ src/Scs/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Scs")]
 [assembly: AssemblyDescription("Simple Client/Server Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Halil ibrahim Kalkan")]
+[assembly: AssemblyCompany("STP")]
 [assembly: AssemblyProduct("Scs")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
